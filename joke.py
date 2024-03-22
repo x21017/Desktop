ebc39615f4439f4c3941b277230961a3e79cd40a
x21017xx <kosukemon@icloud.com>
2024-03-22 07:43:59 +0000

    pythonのコードを作成した

---
+++ joke.py
@@ -0,0 +1,15 @@
+import requests
+
+# ダジャレを取得する関数
+def joke():
+    url = "https://icanhazdadjoke.com/"  # ジョークAPIのURL
+    headers = {"Accept": "application/json"}  # JSON形式のデータを要求するヘッダー
+    response = requests.get(url, headers=headers)  # リクエストを送信してレスポンスを取得
+    joke_data = response.json()  # レスポンスからJSONデータを取得
+    return joke_data["joke"]  # ジョークを返す
+
+
+# この関数を呼び出してジョークを取得することができます
+
+if __name__ == '__main__':
+    print(joke())
---
+++ test.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.Popen(['mpg321', 'test.mp3'])
