f1c2c2dc07a9f354c04e1fad13aa980652717679
x21017xx <kosukemon@icloud.com>
2024-03-25 04:41:18 +0000

    ジョークのIDを取得できるようにした

--- joke.py
+++ joke.py
@@ -1,17 +1,17 @@
 import requests
 
-# ダジャレを取得する関数
+# ジョークを取得する関数
 def joke():
     url = "https://icanhazdadjoke.com/"  # ジョークAPIのURL
     headers = {"Accept": "application/json"}  # JSON形式のデータを要求するヘッダー
     response = requests.get(url, headers=headers)  # リクエストを送信してレスポンスを取得
     joke_data = response.json()  # レスポンスからJSONデータを取得
-    return joke_data["joke"]  # ジョークを返す
+    joke_id = joke_data["id"]  # ジョークのIDを取得
+    return joke_id, joke_data["joke"]  # ジョークとIDを返す
 
 
 # この関数を呼び出してジョークを取得することができます
-
-
-
 if __name__ == '__main__':
-    print(joke())
+    joke_id, joke_text = joke()
+    print("Joke ID:", joke_id)
+    print("Joke:", joke_text)
