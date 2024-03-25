9be724f0e47a52011db77a2200431827e8f7f35e
x21017xx <kosukemon@icloud.com>
2024-03-25 04:53:32 +0000

    コメントの追加

--- joke.py
+++ joke.py
@@ -6,10 +6,10 @@ def joke():
     headers = {"Accept": "application/json"}  # JSON形式のデータを要求するヘッダー
     response = requests.get(url, headers=headers)  # リクエストを送信してレスポンスを取得
     joke_data = response.json()  # レスポンスからJSONデータを取得
-    return joke_data["setup"], joke_data["punchline"], joke_data["id"]  # ジョークを返す
+    return joke_data["setup"], joke_data["punchline"], joke_data["id"]  # ジョークとIDを返す
 
 
-# この関数を呼び出してジョークを取得することができます
+# この関数を呼び出してジョークとIDを取得する
 if __name__ == '__main__':
     setup, punchline, id = joke()
     print("Setup:", setup)
