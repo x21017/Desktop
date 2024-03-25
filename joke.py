b39bfdc34f1153211eefa01726d2041a625e1a4c
x21017xx <kosukemon@icloud.com>
2024-03-25 04:52:38 +0000

    IDを取得できるようにした（２）

--- joke.py
+++ joke.py
@@ -6,11 +6,12 @@ def joke():
     headers = {"Accept": "application/json"}  # JSON形式のデータを要求するヘッダー
     response = requests.get(url, headers=headers)  # リクエストを送信してレスポンスを取得
     joke_data = response.json()  # レスポンスからJSONデータを取得
-    return joke_data["setup"], joke_data["punchline"]  # ジョークを返す
+    return joke_data["setup"], joke_data["punchline"], joke_data["id"]  # ジョークを返す
 
 
 # この関数を呼び出してジョークを取得することができます
 if __name__ == '__main__':
-    setup, punchline = joke()
+    setup, punchline, id = joke()
     print("Setup:", setup)
     print("Punchline:", punchline)
+    print("Id", id)
