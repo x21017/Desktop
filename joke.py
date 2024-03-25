a2e565745277383d3c9f868538c307188e35a64a
x21017xx <kosukemon@icloud.com>
2024-03-25 04:47:54 +0000

    APIを変更した

--- joke.py
+++ joke.py
@@ -2,16 +2,15 @@ import requests
 
 # ジョークを取得する関数
 def joke():
-    url = "https://icanhazdadjoke.com/"  # ジョークAPIのURL
+    url = "https://official-joke-api.appspot.com/jokes/random"  # ジョークAPIのURL
     headers = {"Accept": "application/json"}  # JSON形式のデータを要求するヘッダー
     response = requests.get(url, headers=headers)  # リクエストを送信してレスポンスを取得
     joke_data = response.json()  # レスポンスからJSONデータを取得
-    joke_id = joke_data["id"]  # ジョークのIDを取得
-    return joke_id, joke_data["joke"]  # ジョークとIDを返す
+    return joke_id, joke_data["setup"], joke_data["punchline"]  # ジョークとIDを返す
 
 
 # この関数を呼び出してジョークを取得することができます
 if __name__ == '__main__':
-    joke_id, joke_text = joke()
-    print("Joke ID:", joke_id)
-    print("Joke:", joke_text)
+    joke_id, setup, punchline = joke()
+    print("Setup:", setup)
+    print("Punchline:", punchline)
