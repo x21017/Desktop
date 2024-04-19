5c7f6e01ddaa50309010838d62df01e87667e399
x21017xx <kosukemon@icloud.com>
2024-04-19 05:36:04 +0100

    センサからのデータを取得できるようにした

---
+++ environmental sensor.py
@@ -0,0 +1,22 @@
+import requests
+
+def environmental_sensor_data():
+    url = 'https://sensor-api.sysken.net/get/sensor'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # エラーがあれば例外を発生させる
+        sensor_data = response.json()  # JSONレスポンスをPythonのリストに変換
+        return sensor_data[:17]  # データを返す
+    except requests.exceptions.RequestException as e:
+        print("センサーデータを取得できませんでした:", e)
+        return None
+
+# センサーデータを取得する
+data = environmental_sensor_data()
+if data:
+    print("取得したデータ:", data)
+    # データの処理を行う
+    # 例: データの解析や可視化などを行う
+else:
+    print("データを取得できませんでした")
