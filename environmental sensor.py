bf265c1a60a77e1b9b16cf7804c260291761d535
x21017xx <kosukemon@icloud.com>
2024-04-23 03:33:33 +0100

    定期的に実行されるようにした

--- environmental sensor.py
+++ environmental sensor.py
@@ -1,4 +1,6 @@
 import requests
+import schedule
+import time
 
 def environmental_sensor_data():
     url = 'https://sensor-api.sysken.net/get/sensor'
@@ -12,9 +14,20 @@ def environmental_sensor_data():
         print("センサーデータを取得できませんでした:", e)
         return None
 
-# センサーデータを取得する
-data = environmental_sensor_data()
-if data:
-    print("取得したデータ:", data)
-else:
-    print("データを取得できませんでした")
+def job():
+    data = environmental_sensor_data()
+    if data:
+        print("取得したデータ:", data)
+    else:
+        print("データを取得できませんでした")
+
+# 初回の実行
+job()
+
+# 10秒ごとにジョブをスケジュール
+schedule.every(10).seconds.do(job)
+
+# ループしてスケジュールされたジョブを実行
+while True:
+    schedule.run_pending()
+    time.sleep(1)
