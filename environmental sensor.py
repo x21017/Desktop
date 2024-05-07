3c79c2d74b534cdfcc019af9bbab8509b33f8e84
x21017xx <kosukemon@icloud.com>
2024-05-07 03:00:03 +0100

    温度によって音が流れるようになった

--- environmental sensor.py
+++ environmental sensor.py
@@ -1,6 +1,8 @@
 import requests
 import schedule
 import time
+import schedule
+import subprocess
 
 def environmental_sensor_data():
     url = 'https://sensor-api.sysken.net/get/sensor'
@@ -9,7 +11,7 @@ def environmental_sensor_data():
         response = requests.get(url)
         response.raise_for_status()  # エラーがあれば例外を発生させる
         sensor_data = response.json()  # JSONレスポンスをPythonのリストに変換
-        return sensor_data[:17]  # データを返す
+        return sensor_data[0]['temperature']
     except requests.exceptions.RequestException as e:
         print("センサーデータを取得できませんでした:", e)
         return None
@@ -17,9 +19,17 @@ def environmental_sensor_data():
 def job():
     data = environmental_sensor_data()
     if data:
-        print("データ:", data)
+        # print(len(data))
+        print("取得したデータ:", data)
+        # print("温度:", data)
     else:
         print("データを取得できませんでした")
+# 温度の閾値によって音楽を流す
+    # if data >= 26:
+    #     print("温度が26度以上です.")
+    #     subprocess.Popen(['mpg321', 'test.mp3'])
+    # else:
+    #     print("温度は26度以下です") 
 
 # 初回の実行
 job()
