3f9a78e596f3ca215c3288e45eecbec04c25700f
x21017xx <kosukemon@icloud.com>
2024-05-13 07:19:03 +0100

    温度を音量に変更し、10時から18時の間でしか実行されないようにした

--- environmental sensor.py
+++ environmental sensor.py
@@ -1,8 +1,12 @@
 import requests
 import schedule
 import time
-import schedule
 import subprocess
+from datetime import datetime
+import pytz
+
+# 日本時間のタイムゾーンを設定
+jst = pytz.timezone('Asia/Tokyo')
 
 def environmental_sensor_data():
     url = 'https://sensor-api.sysken.net/get/sensor'
@@ -11,25 +15,28 @@ def environmental_sensor_data():
         response = requests.get(url)
         response.raise_for_status()  # エラーがあれば例外を発生させる
         sensor_data = response.json()  # JSONレスポンスをPythonのリストに変換
-        return sensor_data[0]['temperature']
+        return sensor_data[0]['sound_noise']
     except requests.exceptions.RequestException as e:
         print("センサーデータを取得できませんでした:", e)
         return None
 
 def job():
-    data = environmental_sensor_data()
-    if data:
-        # print(len(data))
-        print("取得したデータ:", data)
-        # print("温度:", data)
+    # 現在の日本時間を取得
+    now = datetime.now(jst)
+    
+    # 日本時間での時間帯が10時から18時の間の場合にのみ処理を実行
+    if 10 <= now.hour < 18:
+        data = environmental_sensor_data()
+        if data:
+            print("取得したデータ:", data)
+            # 音量の閾値によって音楽を流す
+            if data >= 30:
+                print("部屋の中が静かです.")
+                subprocess.Popen(['mpg321', 'test.mp3'])
+        else:
+            print("データを取得できませんでした")
     else:
-        print("データを取得できませんでした")
-# 温度の閾値によって音楽を流す
-    # if data >= 26:
-    #     print("温度が26度以上です.")
-    #     subprocess.Popen(['mpg321', 'test.mp3'])
-    # else:
-    #     print("温度は26度以下です") 
+        print("時刻が18時を過ぎています")
 
 # 初回の実行
 job()
