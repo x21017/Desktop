86db2c792c494bc182331dcb9c547172b652ede7
x21017xx <kosukemon@icloud.com>
2024-05-17 03:35:45 +0100

    部屋の人数を取得できるようにした

--- environmental sensor.py
+++ environmental sensor.py
@@ -20,21 +20,44 @@ def environmental_sensor_data():
         print("センサーデータを取得できませんでした:", e)
         return None
 
+def staywatch_sensor_data():
+    url = 'https://staywatch-backend.kajilab.net/api/v1/stayers'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # エラーがあれば例外を発生させる
+        staywatch_data = response.json()  # JSONレスポンスをPythonのリストに変換
+        return staywatch_data
+    except requests.exceptions.RequestException as e:
+        print("センサーデータを取得できませんでした:", e)
+        return None
+
+def calculate_total_people(staywatch_data):
+    total_people = len(staywatch_data)  # 人数をカウント
+    return total_people
+
 def job():
     # 現在の日本時間を取得
     now = datetime.now(jst)
     
     # 日本時間での時間帯が10時から18時の間の場合にのみ処理を実行
     if 10 <= now.hour < 18:
-        data = environmental_sensor_data()
-        if data:
-            print("取得したデータ:", data)
+        noise_data = environmental_sensor_data()
+        staywatch_data = staywatch_sensor_data()
+        
+        if noise_data:
+            print("取得したデータ:", noise_data)
             # 音量の閾値によって音楽を流す
-            if data >= 30:
+            if noise_data >= 30:
                 print("部屋の中が静かです.")
                 subprocess.Popen(['mpg321', 'test.mp3'])
         else:
             print("データを取得できませんでした")
+
+        if staywatch_data:
+            total_people = calculate_total_people(staywatch_data)
+            print("現在の総人数:", total_people)
+        else:
+            print("人数データを取得できませんでした")
     else:
         print("時刻が10時を回っていないか、18時を過ぎています")
 
